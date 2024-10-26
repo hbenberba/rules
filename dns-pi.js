8f268c4cee21779356342280587ba2dd3f545f27
hbenberba <160870262+hbenberba@users.noreply.github.com>
2024-10-26 19:57:41 +0800

    Update dns-pi.js

--- dns-pi.js
+++ dns-pi.js
@@ -1,4 +1,24 @@
-if ($network?.v4?.primaryRouter === '192.168.31.1') {
+// network:
+// {
+//   "wifi": {
+//     "bssid": "xxx",
+//     "ssid": "xxx"
+//   },
+//   "v4": {
+//     "primaryAddress": "xxx",
+//     "primaryRouter": "xxx",
+//     "primaryInterface": "en0"
+//   },
+//   "dns": [
+//     "xxx"
+//   ],
+//   "v6": {
+//     "primaryAddress": "xxx",
+//     "primaryInterface": "en0"
+//   }
+// }
+
+if ($network?.wifi?.bssid === '3c:cd:57:fa:c7:0d') {
     $done({ address: '192.168.31.148', ttl: 10 });
 } else {
     $done({});
