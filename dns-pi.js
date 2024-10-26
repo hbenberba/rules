eb423f9a9e8ab07ce278b86ccce5ac0740ff52a1
hbenberba <160870262+hbenberba@users.noreply.github.com>
2024-10-26 19:33:53 +0800

    Update dns-pi.js

--- dns-pi.js
+++ dns-pi.js
@@ -1,3 +1,8 @@
+console.log(`dns
+$domain:
+${$domain}
+network:
+${JSON.stringify($network, null, 2)}`);
 if ($network?.v4?.primaryRouter === '192.168.31.1' && $domain === 'l475v.dayelaiwan.com') {
     $done({ addresses: '192.168.31.148', ttl: 10 });
 } else {
